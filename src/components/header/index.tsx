9a6f87c19d28c9c75b84633fec33bb8523e8587f
Fafemi Adeola <adeola5678@gmail.com>
2023-09-22 14:22:22 +0100

    fix: header paddings

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,11 +2,11 @@ import RejuvenateAi from "../../images/svg/rejuvenate-logo.svg";
 
 const Header = () => {
   return (
-    <section className='max-w-[1300px] w-full py-5 flex justify-between items-center mx-auto'>
+    <section className='max-w-[1300px] w-full py-1 flex justify-between items-center mx-auto'>
         <div>
             <RejuvenateAi />
         </div>
-        <button className="bg-[#014421] h-[48px] w-[110px] lg:w-[163px] lg:h-[59px] font-bold text-base lg:text-[20px] text-[#F5F5DC] rounded-xl">
+        <button className="bg-[#014421] h-[48px] w-[110px] lg:w-[163px] lg:h-[50px] font-bold text-base lg:text-[20px] text-[#F5F5DC] rounded-xl">
             Register
         </button>
     </section>
