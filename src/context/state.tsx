37a2ee676074a65eb0c2d73dd4eb4bf36c01afbc
Lashe <adeola5678@gmail.com>
2023-10-15 17:06:24 +0100

    fix: dev

--- src/context/state.tsx
+++ src/context/state.tsx
@@ -1,6 +1,28 @@
 'use client';
 import { createContext, useContext, useState } from 'react';
+import { WagmiConfig, createConfig } from "wagmi";
+import {
+  ConnectKitProvider,
+  getDefaultConfig,
+} from "connectkit";
 import { stateContextType } from '../types/state';
+
+
+const config = createConfig(
+  getDefaultConfig({
+    // Required API Keys
+    alchemyId: process.env.ALCHEMY_ID, // or infuraId
+    walletConnectProjectId: "",
+
+    // Required
+    appName: "Your App Name",
+
+    // Optional
+    appDescription: "Your App Description",
+    appUrl: "https://family.co", // your app's url
+    appIcon: "https://family.co/logo.png", // your app's icon, no bigger than 1024x1024px (max. 1MB)
+  })
+);
 
 const contextDefaultValue: stateContextType = {
   allTokensData: {},
@@ -32,7 +54,11 @@ export function AppWrapper({ children }: StateContextProviderProps) {
   };
 
   return (
-    <AppContext.Provider value={sharedState}>{children}</AppContext.Provider>
+      <WagmiConfig config={config}>
+        <ConnectKitProvider>
+          <AppContext.Provider value={sharedState}>{children}</AppContext.Provider>
+        </ConnectKitProvider> 
+      </WagmiConfig>
   );
 }
 
